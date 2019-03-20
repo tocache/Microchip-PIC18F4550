3d34661da19f728361798d14c109a6eec6a01d57
Kalun Lau <klnlau@gmail.com>
2019-03-20 09:19:59 -0500

    Reorganizado de carpetas
    Reorganizado de carpetas

---
+++ UPC_Summerschool_2019/Myfirstprogram.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Myfirstprogram.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Myfirstprogram.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=myfirstprogram.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/myfirstprogram.x.tar
