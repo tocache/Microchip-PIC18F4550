7b0deb95d6e9d7a629383eb41396877375b6bfa0
Kalun Lau <klnlau@gmail.com>
2019-03-20 09:22:39 -0500

    ah

---
+++ UPC_Summerschool_2019/Timer0_timermode.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Timer0_timermode.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Timer0_timermode.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=timer0timermode.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/timer0timermode.x.tar
