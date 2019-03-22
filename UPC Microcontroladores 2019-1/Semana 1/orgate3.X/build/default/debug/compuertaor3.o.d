308fc0fcc6bbfee543c805a8b1a866740a8e903c
Kalun Lau <klnlau@gmail.com>
2019-03-22 16:52:36 -0500

    LS52 Compuerta OR de tres entradas

---
+++ UPC Microcontroladores 2019-1/Semana 1/orgate3.X/build/default/debug/compuertaor3.o.d
@@ -0,0 +1,3 @@
+build/default/debug/compuertaor3.o:  \
+	E:/Miercocontroleitors/orgate3.X/compuertaor3.asm  \
+
---
+++ UPC Microcontroladores 2019-1/Semana 1/orgate3.X/build/default/production/compuertaor3.o.d
@@ -0,0 +1,3 @@
+build/default/production/compuertaor3.o:  \
+	E:/Miercocontroleitors/orgate3.X/compuertaor3.asm  \
+
