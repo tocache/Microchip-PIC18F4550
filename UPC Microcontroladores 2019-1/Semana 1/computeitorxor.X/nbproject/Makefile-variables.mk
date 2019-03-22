c38339a6da89f4304b95d02f72b2d833c566137c
Kalun Lau <klnlau@gmail.com>
2019-03-21 21:08:08 -0500

    EL52 2
    Compuerta XOR emulado en el PIC18F4550

---
+++ UPC Microcontroladores 2019-1/Semana 1/computeitorxor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=computeitorxor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/computeitorxor.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=computeitorxor.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/computeitorxor.x.tar
