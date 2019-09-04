0ddafce7b8a84d91a24637be912b09174a3edb9e
Kalun Lau <klnlau@gmail.com>
2019-09-03 19:59:31 -0500

    1KHz

---
+++ UPC Microcontroladores 2019-2/Semana 4/cuadradon.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cuadradon.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cuadradon.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cuadradon.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cuadradon.x.tar
