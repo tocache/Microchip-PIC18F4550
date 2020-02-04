8f4a012a430c512c9a8d7301c461f6f09a4db6b6
Kalun Lau <klnlau@gmail.com>
2020-02-04 12:49:48 -0500

    Add files via upload

---
+++ UPC Microcontroladores 2020-0/Semana 5/Ejercicio 12 - PWM.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Ejercicio_12_-_PWM.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Ejercicio_12_-_PWM.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ejercicio12-pwm.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ejercicio12-pwm.x.tar
