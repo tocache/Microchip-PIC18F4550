2a692ada238487f614e7f2f6f637fd0ff561703a
Kalun Lau <klnlau@gmail.com>
2020-02-25 16:48:58 -0500

    Add files via upload

---
+++ UPC Summer School 2020/Summer 04 - Cont 0-9.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Summer_04_-_Cont_0-9.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Summer_04_-_Cont_0-9.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=summer04-cont0-9.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/summer04-cont0-9.x.tar
