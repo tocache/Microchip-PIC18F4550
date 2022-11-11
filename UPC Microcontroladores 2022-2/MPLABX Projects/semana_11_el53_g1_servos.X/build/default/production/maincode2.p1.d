cf55d566d9ed3240cd2e43828b26046781a4fe2b
Kalun Lau <klnlau@gmail.com>
2022-11-11 12:30:53 -0500

    Add files via upload

---
+++ UPC Microcontroladores 2022-2/MPLABX Projects/semana_11_el53_g1_servos.X/build/default/production/maincode2.p1.d
@@ -0,0 +1,4 @@
+build/default/production/maincode2.p1:  \
+maincode2.c  \
+cabecera.h  \
+LCD.h 
---
+++ UPC Microcontroladores 2022-2/MPLABX Projects/semana_11_el53_g1_servos.X/maincode1.c
@@ -0,0 +1,44 @@
+#include <xc.h>
+#include "cabecera.h"
+#include "LCD.h"
+#define _XTAL_FREQ 8000000UL
+
+void configuro(void){
+    OSCCON = 0x70;
+    TRISEbits.RE0 = 0;
+    ADCON1 = 0x0F;
+}
+
+void inicio_LCD(void){
+    TRISD = 0x00;
+    LCD_CONFIG();
+    __delay_ms(23);
+    BORRAR_LCD();
+    CURSOR_HOME();
+    CURSOR_ONOFF(OFF);
+}
+
+
+void main(void) {
+    configuro();
+    inicio_LCD();
+    POS_CURSOR(1,0);
+    ESCRIBE_MENSAJE("Semana 11 servos",16);
+    while(1){
+        POS_CURSOR(2,0);
+        if(PORTCbits.RC1 == 1){
+            ESCRIBE_MENSAJE("LDR destapado",13);
+            LATEbits.LE0 = 1;
+            __delay_us(600);
+            LATEbits.LE0 = 0;
+            __delay_us(19400);
+        }
+        else{
+            ESCRIBE_MENSAJE("LDR tapado   ",13);
+            LATEbits.LE0 = 1;
+            __delay_us(2400);
+            LATEbits.LE0 = 0;
+            __delay_us(17600);
+        }
+    }
+}
